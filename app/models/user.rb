67aba1434b922b64ea23b2b3ca3690679d88ec4b
Oscar Diaz <oscar.diaz@bitsamericas.com>
2022-05-15 17:36:59 -0500

    Crea el modelo user

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  require "securerandom" #para q use el algoritmo bcrypt al guadar en la BD
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :password_digist, presence: true, length: {minimum: 6}
+  validates :username, presence: true, uniqueness: true
+end
