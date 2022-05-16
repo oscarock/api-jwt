85dae1ee53c24378bc63a53b04d9865e84f5db42
Oscar Diaz <oscar.diaz@bitsamericas.com>
2022-05-15 19:43:31 -0500

    validaciones para usar el token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :email, :password_digest)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :password_digist, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}
   validates :username, presence: true, uniqueness: true
 end
